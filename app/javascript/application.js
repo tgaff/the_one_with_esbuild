e051a76d27b5a5cabe545492bdaf33576f5a55af
Travis Gaff <travis@labzero.com>
2023-03-24 13:51:49 +0900

    setup popper tooltip and turbo-stream area for next step

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 // Entry point for the build script in your package.json
+
 import "@hotwired/turbo-rails"
 import "./controllers"
+import * as Popper from "@popperjs/core"
 import * as bootstrap from "bootstrap"
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
